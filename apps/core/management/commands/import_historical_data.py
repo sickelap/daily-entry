7327a44995e47a106d6cf1346b0c41da900a3459
sickelap <sickelap@pm.me>
2025-04-13 20:18:14 +0100

    add import command

---
+++ apps/core/management/commands/import_historical_data.py
@@ -0,0 +1,30 @@
+import csv
+
+from dateutil import parser
+from django.apps import apps
+from django.core.management.base import BaseCommand
+
+WeightEntry = apps.get_model("tracker.WeightEntry")
+User = apps.get_model("core.User")
+
+
+class Command(BaseCommand):
+    help = "Import historic data from CSV file"
+
+    def add_arguments(self, parser):
+        parser.add_argument("csv_file", type=str, help="Path to CSV file")
+        parser.add_argument("user_id", type=int, help="User to import data for")
+
+    def handle(self, *_, **options):
+        user_id = options["user_id"]
+        with open(options["csv_file"], newline="") as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                timestamp = parser.parse(row[0])
+                weight = row[1]
+                user = User.objects.filter(id=user_id).first()
+                self.stdout.write(f"{timestamp}, {weight}")
+                WeightEntry.objects.create(
+                    timestamp=timestamp, weight=weight, user=user
+                )
+        self.stdout.write("Import complete.")
--- apps/tracker/models.py
+++ apps/tracker/models.py
@@ -1,9 +1,11 @@
+from datetime import datetime, timezone
+
 from apps.core.models import User
 from django.db import models
 from django.db.models import CASCADE, ForeignKey, fields
 
 
 class WeightEntry(models.Model):
-    timestamp = fields.DateTimeField(auto_now=True)
+    timestamp = fields.DateTimeField(default=datetime.now(timezone.utc))
     weight = fields.DecimalField(max_digits=5, decimal_places=1, blank=False)
     user = ForeignKey(User, on_delete=CASCADE, related_name="weight_entries")
