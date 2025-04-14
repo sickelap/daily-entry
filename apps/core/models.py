90588888fbd5f62d16597a9e8648f0e3ee75ffb2
sickelap <sickelap@pm.me>
2025-04-14 08:18:16 +0100

    enhance user model

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,5 +1,15 @@
+from zoneinfo import available_timezones
+
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+TIMEZONE_CHOICES = [(tz, tz) for tz in available_timezones()]
 
 
 class User(AbstractUser):
-    pass
+    first_name = models.CharField(max_length=100, default="")
+    last_name = models.CharField(max_length=100, default="")
+    timezone = models.CharField(
+        max_length=50, choices=TIMEZONE_CHOICES, default=settings.TIME_ZONE
+    )
