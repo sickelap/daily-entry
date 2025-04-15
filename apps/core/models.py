1cc45f12ec72bb0d7c4ba4667a5fd83ac361b74c
sickelap <sickelap@pm.me>
2025-04-15 09:53:17 +0100

    admin users and use local time in UI

--- apps/core/models.py
+++ apps/core/models.py
@@ -4,12 +4,12 @@ from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-TIMEZONE_CHOICES = [(tz, tz) for tz in available_timezones()]
+TIMEZONE_CHOICES = sorted((tz, tz) for tz in available_timezones())
 
 
 class User(AbstractUser):
-    first_name = models.CharField(max_length=100, default="")
-    last_name = models.CharField(max_length=100, default="")
+    first_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100, blank=True)
     timezone = models.CharField(
         max_length=50, choices=TIMEZONE_CHOICES, default=settings.TIME_ZONE
     )
