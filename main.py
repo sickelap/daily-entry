31f15b51d7982b54cab26b1b3a8172e90a945a10
sickelap <sickelap@pm.me>
2025-05-24 08:15:02 +0100

    allow valid token

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-from fastapi import FastAPI, HTTPException
+from typing import Annotated, Optional
+
+from fastapi import Depends, FastAPI, Header, HTTPException
 
 app = FastAPI()
 
 
+class AppHeaders:
+    token: str
+
+
+def parse_token(token: Annotated[str | None, Header()] = None) -> str | None:
+    return token
+
+
 @app.get("/stats")
-async def get_stats():
-    raise HTTPException(status_code=403)
+async def get_stats(token=Depends(parse_token)):
+    if not token:
+        raise HTTPException(status_code=403)
+    return [{"_t": 0, "v": 100.9}]
--- test_main.py
+++ test_main.py
@@ -1,9 +1,24 @@
 from fastapi.testclient import TestClient
-from main import app
-
-client = TestClient(app)
+from main import app, parse_token
 
 
 def test_get_stats_anonymous():
+    client = TestClient(app)
     response = client.get("/stats")
     assert response.status_code == 403
+
+
+def test_get_stats_with_token():
+    app.dependency_overrides[parse_token] = lambda: "ok"
+    client = TestClient(app)
+    response = client.get("/stats", headers={"Token": "token"})
+    assert response.status_code == 200
+    app.dependency_overrides.clear()
+
+
+def test_get_stats_with_invalid_token():
+    app.dependency_overrides[parse_token] = lambda: None
+    client = TestClient(app)
+    response = client.get("/stats", headers={"Token": "token"})
+    assert response.status_code == 403
+    app.dependency_overrides.clear()
