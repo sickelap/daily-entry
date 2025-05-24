192c9895f6e419b83cbb80c4eb7f7294132ad18c
sickelap <sickelap@pm.me>
2025-05-24 18:41:57 +0100

    fail under 90 coverage

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+PYTEST_COV_FLAGS="--cov --cov-report=html --cov-report=term --cov-fail-under=90"
+
 if [[ $1 == "watch" ]]; then
   ptw -- --cov --cov-report=html --cov-report=term
 else
