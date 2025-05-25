945e70bbcffddda93fb865a5a97fd04d04cb81e3
sickelap <sickelap@pm.me>
2025-05-25 13:42:37 +0100

    add metrics entity and update table names

--- app/model.py
+++ app/model.py
@@ -9,7 +9,7 @@ from sqlmodel import Field, Relationship, SQLModel
 
 
 class UserEntity(SQLModel, table=True):
-    __tablename__ = "user"  # type: ignore
+    __tablename__ = "users"  # type: ignore
     id: int | None = Field(primary_key=True, default=None)
     token: UUID | None = Field(default_factory=uuid4)
     email: EmailStr = Field(sa_column=Column(String, unique=True, nullable=False))
@@ -17,6 +17,15 @@ class UserEntity(SQLModel, table=True):
     stats: List["StatsEntity"] = Relationship(
         back_populates="user", sa_relationship_kwargs={"lazy": "selectin"}
     )
+    metrics: list["MetricEntity"] = Relationship(back_populates="user")
+
+
+class MetricEntity(SQLModel, table=True):
+    __table_name__ = "metrics"  # type: ignore
+    id: int | None = Field(default=None, primary_key=True)
+    user_id: int | None = Field(default=None, foreign_key="users.id")
+    user: UserEntity | None = Relationship(back_populates="metrics")
+    name: str = Field(index=True)
 
 
 class StatsEntity(SQLModel, table=True):
@@ -24,7 +33,7 @@ class StatsEntity(SQLModel, table=True):
     id: int | None = Field(primary_key=True, default=None)
     timestamp: int = Field(default_factory=lambda: int(datetime.now().timestamp()))
     value: Decimal = Field(default=0, max_digits=4, decimal_places=1)
-    user_id: int | None = Field(default=None, foreign_key="user.id")
+    user_id: int | None = Field(default=None, foreign_key="users.id")
     user: UserEntity | None = Relationship(back_populates="stats")
 
 
