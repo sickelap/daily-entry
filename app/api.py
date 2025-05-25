c1e7e61a625ecb6ed510b1fcf552cfc894187bfc
sickelap <sickelap@pm.me>
2025-05-25 14:47:24 +0100

    refactor service import

--- app/api.py
+++ app/api.py
@@ -1,6 +1,6 @@
 from typing import Annotated, List
 from app import config
-from app.db import get_session
+from app import db
 from app.model import (
     AddStatRequest,
     CreateMetricRequest,
@@ -25,7 +25,7 @@ async def get_stats(user: Annotated[UserEntity, Depends(service.get_user)]):
 @router.post(config.ADD_STAT_URI)
 async def add_stat(
     user: Annotated[UserEntity, Depends(service.get_user)],
-    db: Annotated[Session, Depends(get_session)],
+    db: Annotated[Session, Depends(db.get_session)],
     payload: AddStatRequest,
 ):
     return service.add_user_stat(db, user, payload)
@@ -34,7 +34,7 @@ async def add_stat(
 @router.post(config.IMPORT_STATS_URI)
 async def import_stats(
     user: Annotated[UserEntity, Depends(service.get_user)],
-    db: Annotated[Session, Depends(get_session)],
+    db: Annotated[Session, Depends(db.get_session)],
     payload: List[Stat],
 ):
     return service.import_user_stats(db, user, payload)
@@ -42,7 +42,7 @@ async def import_stats(
 
 @router.post(config.REGISTER_URI)
 async def register(
-    db: Annotated[Session, Depends(get_session)], payload: UserRegisterRequest
+    db: Annotated[Session, Depends(db.get_session)], payload: UserRegisterRequest
 ):
     token = service.create_user(db, payload)
     return Response(status_code=201, headers={config.AUTH_HEADER: token})
@@ -50,7 +50,7 @@ async def register(
 
 @router.post(config.LOGIN_URI)
 async def login(
-    db: Annotated[Session, Depends(get_session)], payload: UserLoginRequest
+    db: Annotated[Session, Depends(db.get_session)], payload: UserLoginRequest
 ):
     token = service.get_user_token(db, payload)
     return {config.AUTH_HEADER: token}
@@ -59,7 +59,7 @@ async def login(
 @router.post(config.REFRESH_TOKEN_URI)
 async def rotate_token(
     user: Annotated[UserEntity, Depends(service.get_user)],
-    db: Annotated[Session, Depends(get_session)],
+    db: Annotated[Session, Depends(db.get_session)],
 ):
     new_token = service.rotate_user_token(db, user)
     return {config.AUTH_HEADER: new_token}
@@ -68,7 +68,7 @@ async def rotate_token(
 @router.post(config.CREATE_METRIC_URI, response_model=MetricResponse)
 async def create_metric(
     user: Annotated[UserEntity, Depends(service.get_user)],
-    db: Annotated[Session, Depends(get_session)],
+    db: Annotated[Session, Depends(db.get_session)],
     payload: CreateMetricRequest,
 ):
     return service.create_metric(db, user, payload)
@@ -77,6 +77,6 @@ async def create_metric(
 @router.get(config.GET_USER_METRICS_URI, response_model=list[MetricResponse])
 async def get_metrics(
     user: Annotated[UserEntity, Depends(service.get_user)],
-    db: Annotated[Session, Depends(get_session)],
+    db: Annotated[Session, Depends(db.get_session)],
 ):
     return service.get_metrics(db, user)
